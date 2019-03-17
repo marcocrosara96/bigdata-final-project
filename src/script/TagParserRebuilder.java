ef20326dfaf9f974d34adc5d8d2f452e464e7135
marcocrosara96 <marcocrosara96@gmail.com>
2019-03-17 14:24:03 +0100

    TagParser fixes

--- src/script/TagParserRebuilder.java
+++ src/script/TagParserRebuilder.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class TagParserRebuilder {
-    public static String input_DIR = "./dataset/TagDataset/0_origin/";
+    public static String input_DIR = "/media/mark/Data/indexes_00000/0-49/";//default = ./dataset/TagDataset/0_origin/
     public static String output_DIR = "./dataset/TagDataset/";
     public static int N = 10;
     public static File[] inputFiles;
@@ -12,27 +12,37 @@ public class TagParserRebuilder {
     public static BufferedWriter[] b_out = new BufferedWriter[N];
 
     public static void main(String[] args) {
+        System.out.println("process --> START");
         inizializeOutputFiles();
         try {
             BufferedReader b_in;
+            int step = 0;
             //Ciclo su tutti i file di input
             for (File f : inputFiles) {
+                int percentage = 100 / inputFiles.length * step;
+                System.out.println(percentage + "% -> Parsing file " + f.getPath());
                 b_in = new BufferedReader(new FileReader(f));
 
                 String readLine = "", printLine = "";
                 //Ciclo su tutte le righe del file
                 while ((readLine = b_in.readLine()) != null) {
-                    int filenum = getNumFile(readLine);
-                    if(filenum >= 0 && filenum < N){
-                        printLine = getUrl(readLine) + '\t' + getLanguages(readLine) + '\t' + getcharset(readLine) + "\n";
-                        b_out[filenum].write(printLine);
+                    try {
+                        int filenum = getNumFile(readLine);
+                        if (filenum >= 0 && filenum < N) {
+                            printLine = getUrl(readLine) + '\t' + getLanguages(readLine) + '\t' + getcharset(readLine) + "\n";
+                            b_out[filenum].write(printLine);
+                        }
+                    }catch(Exception e){
+                        System.out.println("Error skipped, errorcode:" + e.getMessage());
                     }
                 }
+                step++;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         closeAll();
+        System.out.println("process --> END");
     }
 
     public static void inizializeOutputFiles(){
@@ -68,7 +78,7 @@ public class TagParserRebuilder {
         }
     }
 
-    public static int getNumFile(String line){
+    public static int getNumFile(String line) throws Exception{
         int refIndex = line.indexOf(".warc.gz\"");
         int num = Integer.parseInt(line.substring(refIndex-5, refIndex));
         return num;
@@ -79,7 +89,7 @@ public class TagParserRebuilder {
      * @param line
      * @return
      */
-    public static String getUrl(String line){
+    public static String getUrl(String line) throws Exception{
         int startIndex = line.indexOf("{\"url\": \"") + 9;
         int endIndex = line.indexOf("\"", startIndex);
         return line.substring(startIndex, endIndex);
@@ -90,7 +100,7 @@ public class TagParserRebuilder {
      * @param line
      * @return
      */
-    public static String getLanguages(String line){
+    public static String getLanguages(String line) throws Exception{
         if(!line.contains("\"languages\": \""))
             return "-";
         int startIndex = line.indexOf("\"languages\": \"") + 14;
@@ -103,7 +113,7 @@ public class TagParserRebuilder {
      * @param line
      * @return
      */
-    public static String getcharset(String line){
+    public static String getcharset(String line) throws Exception{
         if(!line.contains("\"charset\": \""))
             return "-";
         int startIndex = line.indexOf("\"charset\": \"") + 12;
