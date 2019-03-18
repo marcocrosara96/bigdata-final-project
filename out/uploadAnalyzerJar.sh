d6947fce19ea1ff3ab0c9535c2ccddd649d9d3a5
marcocrosara96 <marcocrosara96@gmail.com>
2019-03-18 11:44:24 +0100

    Test Overleaf bug

--- out/uploadAnalyzerJar.sh
+++ out/uploadAnalyzerJar.sh
@@ -4,3 +4,6 @@ docker cp analyzer.jar cloudera:/DATA
 echo "Eliminazione vecchia versione e upload nuovo analyzer su hdfs"
 docker exec -it cloudera /DATA/uploadAnalyzerToHDFS.sh
 echo "Upload completato"
+
+
+#only test
