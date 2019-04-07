eaa0e9cd662f36f9dd2beeec946fdd5d63537c41
marcocrosara96 <marcocrosara96@gmail.com>
2019-04-07 10:55:42 +0200

    RegExp fix "€"

--- src/map/Map.java
+++ src/map/Map.java
@@ -18,7 +18,7 @@ public class Map extends Mapper<LongWritable, Text, Text, Text> {
     public static final String DICTIONARY_FILENAME = "dictionary.json";
     public static String DICTIONARY_PATH = "/input/dictionary/" + DICTIONARY_FILENAME;
     private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");//Indent. gli spazi vuoti da word a word
-    private static final Pattern WORD_BOUNDARY_V2 = Pattern.compile("[\\[0-9\\]\\s!\\-$%^&*()_+|~=`{}\\[\\]°:\";'<>?,@#.\\/•]+");
+    private static final Pattern WORD_BOUNDARY_V2 = Pattern.compile("[\\[0-9\\]\\s!\\-$€%^&*()_+|~=`{}\\[\\]°:\";'<>?,@#.\\/•]+");
     public static final String WORDS_SPLITTED_TAG = "[WS]";
     private Dictionary dict;
     private LanguageElect langelect;
